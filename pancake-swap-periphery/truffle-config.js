109cc204259e67e9fa1f5526575f306e142c74ca
unknown <webcgi@naver.com>
2021-11-10 11:45:29 +0900

    v3.0

---
+++ pancake-swap-periphery/truffle-config.js
@@ -0,0 +1,105 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * trufflesuite.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */ //npm install @truffle/hdwallet-provider@1.2.3
+ const path = require("path");
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+
+  //contracts_build_directory: path.join(__dirname, "birb-dapp-ui/src/contracts"),
+
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+    // Another network with more advanced options...
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    // bsc: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+    //   network_id: 56,
+    //   confirmations: 10,
+    //   timeoutBlocks: 200,
+    //   skipDryRun: true
+    // },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+       version: "0.6.5",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
+
+// PancakeRouter  │ '0x0fe3e905ECD3d11DED8fC1F26255b792Df454a3E' │
+// │ PancakeMigrator │ '0x3571612Ab2a7fEbEBd5624f6D96a3D8DCD53fe7b' │
+// │   Migrations    │ '0xd03F443BAf430f551EC216Dcbe56F596435b27cb' │
+// │      WETH       │ '0x8E9C69de0f5c6Aa317e9cE37D72Fe1d485B72345' │
+// │      WBNB       │ '0x5920a680241fD8942073fF74eBab3D1856E9E64A'
